637a0ebdd1f126e9d194ab5339d5282cd60cf1e1
konarskm7775 <160173059+konarskm7775@users.noreply.github.com>
2024-02-15 14:36:27 -0500

    Add files via upload

---
+++ P1LAB1_KonarskaMarta..py
@@ -0,0 +1,14 @@
+#Marta Konarska
+#02/15/2023
+#PILAB1
+#This program will display my name in a welcome message.
+
+#Get the first name from the user and assign it to a variable.
+
+fname =input("Enter your first name: ")
+#print(fname)
+lname =input("Enter your last name: ")
+#print(lname)
+#print out the names in a welcome message
+print("Hello," ,fname,lname + "! Welcome to CTI-110")
+
