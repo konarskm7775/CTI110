cbed0acb4eab53d4f7048d8c9e8129f28d99eb27
konarskm7775 <160173059+konarskm7775@users.noreply.github.com>
2024-02-27 14:00:21 -0500

    Add files via upload
    P2LAB1

---
+++ p2lab1.py
@@ -0,0 +1,14 @@
+#Marta Konarska
+#02/27/2024
+#P2LAB1
+import math
+radius = float(input("What is  the radius of the circle "))
+diameter = 2 * radius
+print('The diameter of the circle is ', diameter)
+##print('The diameter of the circle is ', 2 * radius)
+circumference = 2*3.142*radius
+print('The circumference of the circle is ', circumference)
+##print('The circumference of the circle is ', 2*3.142*radius)
+area = 3.14*math.pow(radius,2)
+##area = 3.14*radius * radius
+print('The area of the circle is ', area)
